caddd6de4f3a703499be71e8eccb6e59fddef8da
nicolus888 <nicola.edgardo@gmail.com>
2022-11-08 16:42:39 -0300

    100 to 0 exercise

---
+++ 100to0.py
@@ -0,0 +1,14 @@
+#make a program that prints 100 to 0 in inversed order.
+#two different solutions...
+
+count = 100
+
+while count < 0:
+    print(count)
+    count -= 1
+
+#-------------------------------
+rango_1_100 = range(1,101)       #see how we can save a range in a variable, awesome.
+
+for i in reversed(rango_1_100):  #and how we literally say to the for loop to go backwards.
+    print(f"- {i}")
