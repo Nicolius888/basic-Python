33d1b9e15e9d22597c3a33cbb8c38f407e6a0601
nicolus888 <nicola.edgardo@gmail.com>
2022-11-08 00:58:56 -0300

    finish contorl flow

--- flow-control.py
+++ flow-control.py
@@ -51,7 +51,7 @@ print("False xor False: ", False ^ False)
 #Its onlt True, when both elements are diferrent
 
 
-####IF STATEMENTS
+####### IF STATEMENTS
 
 a=5
 b=6
@@ -76,7 +76,7 @@ else:
     print("defaulta case")
 
 
-##while
+############# WHILE LOOPS
 
 count = 0
 
@@ -103,3 +103,104 @@ while count <= 35:
         continue
     else: print(count, "is odd number")
     count += 1
+
+
+##########FOR LOOPS
+
+list = [1,2,3,4,5,6,7,8,9,10]
+
+#iterate over a list
+for element in list:
+    print("iterarion number: ", element)
+
+#give a range of numbers
+for number in range(5):
+    print(number) #0,1,2,3,4
+
+for number in range(5, 10):
+    print("hey!",number) #5,6,7,8,9
+
+#range for iterate over a list (or a tuple)
+
+abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+length = len(abc) #reserved word to get the length of the list
+
+for el in range(5,length):
+    print(abc[el]) #f g h i j
+
+################################
+
+strings = ("hello", "world", "python", "javascript")
+
+for el in strings:
+    print("actual word: ", el)
+
+    if el == "world":
+        print("We found World!")
+        break #as we found the word, break out of the loop
+    
+###but, look how powerfull is IN
+
+if "world" in strings:
+    print("We found World!")
+
+if "java" not in strings:
+    print("where is java?")
+
+###### Other cool python things:
+
+#how to order a list?
+
+lista = [3,7,4,8,4,7,9,10]
+
+orderedLista = sorted(lista)
+
+print(orderedLista) #[3, 4, 4, 7, 7, 8, 9, 10] see how we dont loose repeated numbers as in sets
+
+#ordeder and backwards 
+
+orderedAndBack = sorted(lista, reverse=True)
+
+print(orderedAndBack)
+
+abclist = ["A", "c","b","C","B","a"]
+
+orderedABC = sorted(abclist)
+
+print(orderedABC)#['A', 'B', 'C', 'a', 'b', 'c'], remember this works in ASCII
+
+######MATCH its like switch, and it requires python 3.10 or newer
+value=4
+#match value:
+#    case 1:
+#        print("value is 1")
+#    case 4:
+#        print("value is 4!")
+
+
+#### Another couriosity of for loops, The Else in for loops!
+
+list1 = ["hello","hola","python","world"]
+
+for el in list1:
+    if el == "python":
+        print("found it!")
+        break
+else:
+    print("not found :(")
+
+
+#this help to avoid things like creating variables to check the not found cases
+
+##### PASS keyword
+
+for el in list1:
+    pass
+
+#i.e.: like saying, hey python dont worry about this i'm gonna write it later.
+
+if 5>4:
+    pass
+
+#is for not getting error in still not developed parts of your code
