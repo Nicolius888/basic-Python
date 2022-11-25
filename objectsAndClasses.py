f9f4dcec5078c0a990296188120a10bb94330a4a
nicolus888 <nicola.edgardo@gmail.com>
2022-11-24 22:41:44 -0300

    about inheritance

--- objectsAndClasses.py
+++ objectsAndClasses.py
@@ -23,16 +23,22 @@ class Object:
         #without it we are declaring a new variable with the same name
         #but just existent inside the scope of this function.
 
+
+    def checkPrivate(self):
+        return self._myPrivateVariable
+
+
+
 #new instance of the class or new Object
 newObject = Object()
 
 #using the methods
-newObject.changeSomething()
+newObject.changeSomething() #hi
 
 
 #accesing the properties
-print(newObject.property0)
-print(newObject.property1)
+print(newObject.property0) #False
+print(newObject.property1) #True
 
 
 #Python doesn't have the concepts of public, private or protected.
@@ -41,8 +47,8 @@ print(newObject.property1)
 newObject.property0 = True
 newObject.property1 = False
 
-print(newObject.property0)
-print(newObject.property1)
+print(newObject.property0) #True
+print(newObject.property1) #False
 
 #There is a CONVENTION, and it is just a way of NAMING VARIABLES
 #to undesrstand something as "private"
@@ -50,11 +56,49 @@ print(newObject.property1)
 # like _variableName
 #but remember, ITS JUST A CONVENTION.
 
-print(newObject._myPrivateVariable)
+print(newObject._myPrivateVariable) #this is private
 
 #the idea is to not modify any variable that starts with underscore. 
 #but its still techincally possible.
 
 #The correct way of modifying the private variable is with adding a method to do the work.
 newObject.modifyPrivate("car")
-print(newObject._myPrivateVariable)
+print(newObject._myPrivateVariable) #this car is private
+
+#Even to accesing it the ideal is to have a method to not do it directly.
+print(newObject.checkPrivate()) #this car is private
+
+
+#-------------------------------------------------------------------------------------
+
+
+#There is static classes too
+#here all the instances chare the same memory space
+#i.e. all the new instances will have the same properties.
+
+class Static:
+    num  = 0 
+
+    def add():
+        Static.num += 1 #this type of class dont use self
+                        #instead, they call the name of the class to modify properties.
+
+print(Static.num) #0
+Static.add()
+print(Static.num) #1
+
+#----------------------------------------------------------------------------------
+#Inheritance
+
+class myBoolean(Object): #just pass the Father class as an argument
+
+    def checkBoolean(self):
+        pass
+
+newBool = myBoolean()
+
+print(newBool.checkPrivate()) #this is private
+newBool.modifyPrivate("boolean")
+print(newBool.checkPrivate()) #this boolean is private
+
+
